13a25cfca4e0d1fe7ceaffe27c850a527e09a83c
Ondrej Zara <ondrej.zara@firma.seznam.cz>
2014-09-11 15:57:42 +0200

    updating to latest spec+tests

--- promise.js
+++ promise.js
@@ -11,6 +11,7 @@
 var Promise = function(resolver) {
 	this._state = 0; /* 0 = pending, 1 = fulfilled, 2 = rejected */
 	this._value = null; /* fulfillment / rejection value */
+	this._timeout = null;
 
 	this._cb = {
 		fulfilled: [],
@@ -35,11 +36,9 @@ Promise.prototype.then = function(onFulfilled, onRejected) {
 
 	this._thenPromises.push(thenPromise);
 
-	if (this._state > 0) {
-		setTimeout(this._processQueue.bind(this), 0);
-	}
+	if (this._state > 0) { this._schedule(); }
 
-	/* 3.2.6. then must return a promise. */
+	/* 2.2.7. then must return a promise. */
 	return thenPromise; 
 }
 
@@ -53,7 +52,7 @@ Promise.prototype.fulfill = function(value) {
 	this._state = 1;
 	this._value = value;
 
-	this._processQueue();
+	if (this._thenPromises.length) { this._schedule(); }
 
 	return this;
 }
@@ -68,9 +67,37 @@ Promise.prototype.reject = function(value) {
 	this._state = 2;
 	this._value = value;
 
-	this._processQueue();
+	if (this._thenPromises.length) { this._schedule(); }
 
 	return this;
+}
+
+Promise.prototype.resolve = function(value) {
+	if (value == this) {
+		/* 2.3.1. If promise and x refer to the same object, reject promise with a TypeError as the reason. */
+		this.reject(new TypeError("Promise resolved by its own instance"));
+	} else if (value instanceof this.constructor) {
+		/* 2.3.2. If x is a promise, adopt its state */
+		value.chain(this);
+	} else if (value !== null && (typeof(value) == "object" || typeof(value) == "function")) {
+		/* 2.3.3. Otherwise, if x is an object or function,  */
+		try {
+			var then = value.then;
+			if (typeof(then) == "function") {
+				/* 2.3.3.3. If then is a function, call it */
+				then.call(value, this.resolve.bind(this), this.reject.bind(this));
+			} else {
+				/* 2.3.3.4 If then is not a function, fulfill promise with x */
+				return this.fulfill(value);
+			}
+		} catch (e) {
+			/* 2.3.3.2. If retrieving the property x.then results in a thrown exception e, reject promise with e as the reason. */
+			return this.reject(e);
+		}
+	} else {
+		/* 2.3.4. If x is not an object or function, fulfill promise with x. */ 
+		return this.fulfill(value);
+	}
 }
 
 /**
@@ -89,7 +116,14 @@ Promise.prototype["catch"] = function(onRejected) {
 	return this.then(null, onRejected);
 }
 
+Promise.prototype._schedule = function() {
+	if (this._timeout) { return; } /* resolution already scheduled */
+	this._timeout = setTimeout(this._processQueue.bind(this), 0);
+}
+
 Promise.prototype._processQueue = function() {
+	this._timeout = null;
+
 	while (this._thenPromises.length) {
 		var onFulfilled = this._cb.fulfilled.shift();
 		var onRejected = this._cb.rejected.shift();
@@ -102,10 +136,10 @@ Promise.prototype._executeCallback = function(cb) {
 
 	if (typeof(cb) != "function") {
 		if (this._state == 1) {
-			/* 3.2.6.4. If onFulfilled is not a function and promise1 is fulfilled, promise2 must be fulfilled with the same value. */
+			/* 2.2.7.3. If onFulfilled is not a function and promise1 is fulfilled, promise2 must be fulfilled with the same value. */
 			thenPromise.fulfill(this._value);
 		} else {
-			/* 3.2.6.5. If onRejected is not a function and promise1 is rejected, promise2 must be rejected with the same reason. */
+			/* 2.2.7.4. If onRejected is not a function and promise1 is rejected, promise2 must be rejected with the same reason. */
 			thenPromise.reject(this._value);
 		}
 		return;
@@ -113,23 +147,13 @@ Promise.prototype._executeCallback = function(cb) {
 
 	try {
 		var returned = cb(this._value);
-
-		if (returned && typeof(returned.then) == "function") {
-			/* 3.2.6.3. If either onFulfilled or onRejected returns a promise (call it returnedPromise), promise2 must assume the state of returnedPromise */
-			var fulfillThenPromise = function(value) { thenPromise.fulfill(value); }
-			var rejectThenPromise = function(value) { thenPromise.reject(value); }
-			returned.then(fulfillThenPromise, rejectThenPromise);
-		} else {
-			/* 3.2.6.1. If either onFulfilled or onRejected returns a value that is not a promise, promise2 must be fulfilled with that value. */ 
-			thenPromise.fulfill(returned);
-		}
-
 	} catch (e) {
-
-		/* 3.2.6.2. If either onFulfilled or onRejected throws an exception, promise2 must be rejected with the thrown exception as the reason. */
+		/* 2.2.7.2. If either onFulfilled or onRejected throws an exception, promise2 must be rejected with the thrown exception as the reason. */
 		thenPromise.reject(e); 
-
 	}
+
+	/* 2.2.7.1. If either onFulfilled or onRejected returns a value x, run the Promise Resolution Procedure [[Resolve]](promise2, x). */
+	thenPromise.resolve(returned);
 }    
 
 Promise.prototype._invokeResolver = function(resolver) {
